ce4938132b569c61bcd8d60535090ef05c67a966
vineet <vineet@inventics.tech>
2021-12-14 11:44:38 +0000

    doctypes

---
+++ fleet/fleet/doctype/area_name/area_name.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Shivansh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Area Name', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ fleet/fleet/doctype/trip_sheet/trip_sheet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Shivansh and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Trip Sheet', {
+	// refresh: function(frm) {
+
+	// }
+});
